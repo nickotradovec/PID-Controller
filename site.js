97bfb34068ee6bddd1a4fabd644ad39fabe04bd6
Nick Otradovec <njopq8@mst.edu>
2018-11-29 18:44:46 -0500

    Plot Testing

---
+++ site.js
@@ -0,0 +1,21 @@
+﻿function getData() {
+    return Math.random();
+}
+Plotly.plot('chart', [{
+    y: [getData()],
+    type: 'line'
+}]);
+
+var cnt = 0;
+setInterval(function () {
+    Plotly.extendTraces('chart', { y: [[getData()]] }, [0]);
+    cnt++;
+    if (cnt > 300) {
+        Plotly.relayout('chart', {
+            xaxis: {
+                range: [cnt - 300, cnt]
+            }
+        });
+    }
+
+}, 20);
