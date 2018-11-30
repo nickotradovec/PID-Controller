dd96d0bf2708333d592fde44c28736556f1c5453
Nick Otradovec <njopq8@mst.edu>
2018-11-29 22:48:09 -0500

    Random Signal Working

--- site.js
+++ site.js
@@ -6,10 +6,50 @@ var displayPoints = displayTimeSec * 1000 / screenUpdateIntervalMilliSec;
 var desiredPIDPollingRate = 200;
 var usePIDIntervalMilliSec = getPIDPollingRate();
 
+let Wave = {
+    period: 0,
+    amplitude: 0,
+    offset: 0,
+    frequency: function() {
+        return 1 / this.period;
+    }
+}
+
+let ExternalForce = {
+    currentExternalForce: 0,
+    waveComponents: 10,
+    arrayRandomWaves: [],
+
+    GenerateRandomSignal: function(minPeriod, maxPeriod, maxAmplitude) {
+        
+        for(i=0; i<this.waveComponents; i++) {
+            //consider restricting wave frequencies as a function of amplitude
+            var waveComponent = Object.create(Wave);
+            waveComponent.amplitude = (maxAmplitude / Math.sqrt(this.waveComponents)) * Math.random();
+            waveComponent.period = (maxPeriod - minPeriod) * Math.random() + minPeriod;
+            waveComponent.offset = Math.random() * waveComponent.frequency();
+            this.arrayRandomWaves.push(waveComponent)
+        }
+    },
+
+    GetExternalForce: function(time) {
+        // update currentExternalForce
+        var force = 0;
+        for(i=0; i<this.arrayRandomWaves.length; i++) {
+            force += this.arrayRandomWaves[i].amplitude * Math.sin((time * this.arrayRandomWaves[i].frequency() - this.arrayRandomWaves[i].offset) / (2 *Math.PI));
+        }
+        this.currentExternalForce = force;
+        return this.currentExternalForce;
+    }
+}
+
+let testSignal = new Object(ExternalForce);
+testSignal.GenerateRandomSignal(1, 30, 10);
+
 $("#pausebutton").click(function() {blnPaused = !blnPaused;});
 
 Plotly.plot('chart', [{
-    y: [getData()],
+    y: [testSignal.GetExternalForce(0)],
     type: 'line',
     name: 'test1'
 }]);
@@ -19,8 +59,8 @@ var cnt = 0;
 setInterval(function () {
 
     if (!blnPaused) {
-
-        Plotly.extendTraces('chart', { y: [[getData()]] }, [0]);
+        //testGetConstant()
+        Plotly.extendTraces('chart', { y: [[testSignal.GetExternalForce(cnt)]] }, [0]);
 
         cnt++;
         if (cnt > displayPoints) {
@@ -46,7 +86,12 @@ function getData() {
     return Math.random();
 }
 
-var Controller = {
+let CurrentState = {
+    currentTime: 0,
+    currentPostion: 0
+}
+
+let Controller = {
     tuneP: 0,
     tuneI: 0,
     tuneD: 0,
@@ -57,19 +102,14 @@ var Controller = {
     }
 }
 
-var Model = {
+let Model = {
     mass: 0,
     GetNewState: function(timeInterval, externalForce, internalForce) {
 
     }
 }
 
-var ExternalForce = {
-    maxFrequency: 0,
-    maxMagnitude: 0,
-    currentExternalForce,
-    GetExternalForce: function(time) {
-        // update currentExternalForce
-    }
+function testGetConstant() {
+    return 3;
 }
 
