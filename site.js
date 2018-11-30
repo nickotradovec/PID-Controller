0179c787c12e9a0fca50401949e4d79f793fad66
Nick Otradovec <njopq8@mst.edu>
2018-11-29 19:14:26 -0500

    Start/Stop

--- site.js
+++ site.js
@@ -1,21 +1,34 @@
 ﻿function getData() {
     return Math.random();
 }
+
+var blnPaused = false;
+
+document.getElementById("pausebutton").addEventListener("click", function () {
+    blnPaused = !blnPaused;
+});
+
 Plotly.plot('chart', [{
     y: [getData()],
-    type: 'line'
+    type: 'line',
+    name: 'test1'
 }]);
 
 var cnt = 0;
 setInterval(function () {
-    Plotly.extendTraces('chart', { y: [[getData()]] }, [0]);
-    cnt++;
-    if (cnt > 300) {
-        Plotly.relayout('chart', {
-            xaxis: {
-                range: [cnt - 300, cnt]
-            }
-        });
-    }
 
+    if (!blnPaused) {
+
+        Plotly.extendTraces('chart', { y: [[getData()]] }, [0]);
+
+        cnt++;
+        if (cnt > 300) {
+            Plotly.relayout('chart', {
+                xaxis: {
+                    range: [cnt - 300, cnt]
+                }
+            });
+        };
+
+    };
 }, 20);
